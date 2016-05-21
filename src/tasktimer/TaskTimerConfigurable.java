a36368935d5e0dc294e50a328e3a0ee67fb75842
Marek Kisiel <marekkisiel.dev@gmail.com>
2016-05-21 13:02:39 +0200

    #2 created project configuration class

---
+++ src/tasktimer/TaskTimerConfigurable.java
@@ -0,0 +1,51 @@
+package tasktimer;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Created by drupality on 5/17/16.
+ */
+public class TaskTimerConfigurable implements Configurable {
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "Task Timer Settings";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        return null;
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+}
